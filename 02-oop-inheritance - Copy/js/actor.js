2777017463eaf4f3966fbc1247e17403ea1eb76e
Nicolás Cassulo <nicolascassulo@gmail.com>
2017-05-08 17:24:51 -0300

    Fixed folders

--- 02-oop-inheritance - Copy/js/actor.js
+++
@@ -1,7 +0,0 @@
-/*Exercise 7*/
-
-export default class Actor {
-	constructor(name,lastName){
-		this.name = name;
-	}
-}
--- 02-oop-inheritance - Copy/js/logger.js
+++
@@ -1,9 +0,0 @@
-/*Exercise 5*/
-
-export default class Logger {
-
-	log(info){
-		console.log("some info");
-	}
-
-}
